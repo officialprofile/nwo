b46bcfb3eb1109690202da1596c9b76799e3904a
officialprofile <75314157+officialprofile@users.noreply.github.com>
2022-08-25 18:21:38 +0200

    😬

--- lib/nauka.dart
+++ lib/nauka.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:math';
+import 'dart:convert';
 
 List names = ['jeden', 'dwa', 'trzy'];
+var list = [];
+
+Future<void> readJson() async {
+  final String response = await rootBundle.loadString('lib/slownik.json');
+  final data = await json.decode(response);
+  list = data;
+}
 
 class Nauka extends StatefulWidget {
   Nauka({Key? key}) : super(key: key);
@@ -11,9 +20,39 @@ class Nauka extends StatefulWidget {
 }
 
 class _NaukaState extends State<Nauka> {
+  String _haslo = "Nauka wyrazów obcych";
+  String _opis = "Kliknij na przycisk poniżej, aby wylosować pierwsze hasło.";
+
+  void _incrementCounter() {
+    setState(() {
+      final _random = new Random();
+      _haslo = list[_random.nextInt(list.length)]["term"];
+      _opis = list[_random.nextInt(list.length)]["desc"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _random = new Random();
-    return Text(names[_random.nextInt(names.length)]);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _haslo,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            Text(
+              _opis,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () => list.isEmpty ? readJson() : _incrementCounter(),
+        child: const Text('Losuj nowe hasło'),
+      ),
+    );
   }
 }
